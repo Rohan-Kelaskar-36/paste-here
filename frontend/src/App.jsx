32fcab0b2ce372975626248f633bd47daa608b89
Rohan Kelaskar <rohankelaskar127@gmail.com>
2026-01-29 01:12:27 +0530

    Use HashRoute

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,11 @@
 import { HashRouter, Routes, Route } from "react-router-dom";
-
+import Home from "./pages/Home";
+import ViewPaste from "./pages/ViewPaste";
+import { Toaster } from "react-hot-toast";
 function App() {
   return (
     <HashRouter>
+      <Toaster position="top-right" />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/p/:id" element={<ViewPaste />} />
